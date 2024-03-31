ae9483103a6b7be4a8d7d9fa94ddd6075193ce6e
Gary Kim <gary@garykim.dev>
2024-03-30 20:54:05 -0400

    deps: add pandas
    Signed-off-by: Gary Kim <gary@garykim.dev>

--- gkjh/misc.py
+++ gkjh/misc.py
@@ -36,7 +36,7 @@ from .expr_formatting import round_expr
 
 
 def package_versions(
-    packages=["gkjh", "python", "sympy", "matplotlib"],
+    packages=["gkjh", "python", "sympy", "matplotlib", "pandas"],
     header_and_footer=True,
     fatal=False,
 ) -> str:
